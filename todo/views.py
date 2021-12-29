a0964977628ddcd82a13ff77d87a42fd8be98357
risa mehara <r.mehara.224@gmail.com>
2021-12-30 04:29:08 +0900

    create todo outline and visualize with bootstrap

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Todo
 
-def index(request):
-  return HttpResponse('<h1>Hello World </h1>')
+class TodoList(ListView):
+  model = Todo
+  context_object_name = 'tasks'
+
+class TodoDetail(DetailView):
+  model = Todo
+  context_object_name='task'
+
+class TodoCreate(CreateView):
+  model = Todo
+  fields = '__all__'
+  success_url = reverse_lazy('list')
+
+class TodoUpdate(UpdateView):
+  model = Todo
+  fields = '__all__'
+  success_url = reverse_lazy('list')
+
+class TodoDelete(DeleteView):
+  model = Todo
+  context_object_name = 'task'
+  success_url = reverse_lazy('list')
